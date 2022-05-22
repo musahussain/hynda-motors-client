f342b9b4c8f69271f7b6777f0562d0e5104732a9
musahussain <mdmuslimhussain1@gmail.com>
2022-05-22 18:25:07 +0600

    added firebase

--- src/Pages/Home/Product.js
+++ src/Pages/Home/Product.js
@@ -3,22 +3,22 @@ import React from "react";
 const Product = ({pd}) => {
     const {name, img, type, material, position, price} = pd;
   return (
-    <div class="card w-96 mx-auto bg-base-100 shadow-xl">
-      <figure class="px-10 pt-10">
+    <div className="card w-96 mx-auto bg-base-100 shadow-xl">
+      <figure className="px-10 pt-10">
         <img
           src={img}
           alt="Shoes"
-          class="rounded-xl"
+          className="rounded-xl"
         />
       </figure>
-      <div class="card-body items-center text-center">
-        <h2 class="card-title">{name}</h2>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">{name}</h2>
         <p><span className="font-bold">Type:</span> {type}</p>
         <p><span className="font-bold">Material:</span> {material}</p>
         <p><span className="font-bold">Position:</span> {position}</p>
         <p><span className="font-bold">Price:</span> {price}</p>
-        <div class="card-actions">
-          <button class="btn btn-primary text-white">Buy Now</button>
+        <div className="card-actions">
+          <button className="btn btn-primary text-white">Buy Now</button>
         </div>
       </div>
     </div>
